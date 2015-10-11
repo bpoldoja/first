c57fb29473b30238c4c4702f4c4ddeb227256263
Berit <berit642@gmail.com>
2015-10-11 16:52:11 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-/**
- * Created by USER on 19.09.2015.
- */
 
+//Takes to-do label as parameter.
+//Creates a new li with a span and button.
 function createNewTodo(lbl) {
 
     //Make div
@@ -25,11 +24,13 @@ function createNewTodo(lbl) {
     return li;
 }
 
+//Adds new to-do item
 function addNewTodo(newTodo) {
     var todo_list = document.getElementById("todo-list");
     todo_list.appendChild(newTodo);
 }
 
+//Makes a new lbl and returns the input empty
 function getToDoLbl() {
     var todo_input = document.getElementById("todo-input");
     var val = todo_input.value;
@@ -37,10 +38,12 @@ function getToDoLbl() {
     return val;
 }
 
+// Removes an element
 function removeToDo(element) {
     element.parentNode.removeChild(element);
 }
 
+//When clicked + button creates new to-do li
 function onAddClicked() {
     var newToDoLbl = getToDoLbl();
     var newTodo = createNewTodo(newToDoLbl);
